d34e9942411d054c60ea6ed6a8cbc5524d45a478
aero <abdullah.qalam@ozu.edu.tr>
2024-12-14 13:02:41 +0300

    Finalised preliminary version of the lexer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use std::fs;
 use thiserror::Error;
+use std::fmt;
+
 enum Expr<'a> {
     Atom(&'a str),
     List(Vec<Expr<'a>>),
+}
+
+impl<'a> fmt::Debug for Expr<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Expr::Atom(ref s) => write!(f, "Atom({})", s),
+            Expr::List(ref l) => write!(f, "List({:?})", l),
+        }
+    }
 }
 
 #[derive(Error, Debug)]
 enum LexError {
     #[error("Failed to open file: {0}")]
     FileNotFound(String),
+    #[error("Mismatched parentheses, please make sure your parentheses are paired")]
+    UnpairedParentheses(),
 }
 
 fn file_reader(input: &str) -> Result<String, LexError> {
@@ -35,10 +48,37 @@ fn tokeniser(s: &str) -> Vec<&str> {
             start = i + 1;
         }
     }
-    if start < s.len() {tokens.push(&s[start..]);}
+    if start < s.len() {
+        tokens.push(&s[start..]);
+    }
     tokens
 }
-
+fn parser(v: Vec<&str>) -> Result<Vec<Expr<'_>>, LexError> {
+    let mut output: Vec<Expr<'_>> = Vec::new();
+    let mut stack = Vec::new();
+    let mut in_list = false;
+    for (index, &value) in v.iter().enumerate() {
+        if value == "(" {
+            stack.push(index);
+            in_list = true;
+        } else if value == ")" {
+            let open_index = stack.pop().ok_or_else(|| LexError::UnpairedParentheses())?;
+            output.push(Expr::List(
+                v[open_index + 1..index]
+                    .iter()
+                    .map(|s| Expr::Atom(*s))
+                    .collect(),
+            ));
+            in_list = false;
+        } else if !in_list {
+            output.push(Expr::Atom(value))
+        }
+    }
+    if !stack.is_empty() {
+        return Err(LexError::UnpairedParentheses());
+    }
+    Ok(output)
+}
 fn main() {
     let input = "src/test.txt";
     let strings = match file_reader(input) {
@@ -48,6 +88,11 @@ fn main() {
             String::new()
         }
     };
-   println!("{:?}", tokeniser(&strings));
-
+    let tokens = tokeniser(&strings);
+    println!("{:?}", tokeniser(&strings));
+    let lexed = match parser(tokens) {
+        Ok(n) => n,
+        Err(e) => return println!("Error: {e}"),
+    };
+    println!("{:?}", lexed);
 }
